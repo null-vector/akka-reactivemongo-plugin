0b962039de1807bcd98c26a8d663d3a88383d412
rodrigo <rodrigo.gdea@gmail.com>
2020-03-09 14:59:53 -0300

    Using macros to create EventAdapters (WIP)

---
+++ api/src/main/scala/org/nullvector/AdapterKey.scala
@@ -0,0 +1,8 @@
+package org.nullvector
+
+case class AdapterKey(payloadType: Class[_]) {
+
+  override def hashCode(): Int = payloadType.getPackage.hashCode()
+
+  override def equals(obj: Any): Boolean = payloadType.isAssignableFrom(obj.asInstanceOf[AdapterKey].payloadType)
+}
