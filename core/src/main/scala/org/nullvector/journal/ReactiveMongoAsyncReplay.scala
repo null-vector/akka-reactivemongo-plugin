ff016899fbcf5560954f48f1eae67c413652a401
Julian Sarrelli <julian.sarrelli@tiendanube.com>
2021-05-13 20:52:43 -0300

    refactor asyncReplayMessagesç

--- core/src/main/scala/org/nullvector/journal/ReactiveMongoAsyncReplay.scala
+++ core/src/main/scala/org/nullvector/journal/ReactiveMongoAsyncReplay.scala
@@ -1,11 +1,8 @@
 package org.nullvector.journal
 
 import akka.persistence.PersistentRepr
-import akka.stream.{ActorAttributes, Materializer}
 import org.nullvector.Fields
 import org.nullvector.ReactiveMongoDriver.QueryType
-import org.nullvector.ReactiveMongoPlugin.pluginDispatcherName
-import org.nullvector.bson.BsonTextNormalizer
 import org.nullvector.logging.LoggerPerClassAware
 import reactivemongo.akkastream.cursorProducer
 import reactivemongo.api.bson._
@@ -17,57 +14,55 @@ import scala.util.{Failure, Success}
 trait ReactiveMongoAsyncReplay extends LoggerPerClassAware {
   this: ReactiveMongoJournalImpl =>
 
-  val parallelism = Runtime.getRuntime.availableProcessors()
-  private implicit lazy val mat: Materializer = Materializer.matFromSystem(actorSystem)
-
   def asyncReplayMessages(persistenceId: String, fromSequenceNr: Long, toSequenceNr: Long, max: Long)
                          (recoveryCallback: PersistentRepr => Unit): Future[Unit] = {
     logger.debug(s"[[Roro]] Recovering events for {} from {} to {}", persistenceId, fromSequenceNr, toSequenceNr)
-    rxDriver.journalCollection(persistenceId).flatMap { collection: BSONCollection =>
+
+    def buildQuery(collection: BSONCollection) = {
       val query = BSONDocument(
         Fields.persistenceId -> persistenceId,
         Fields.to_sn -> BSONDocument("$gte" -> fromSequenceNr),
         Fields.from_sn -> BSONDocument("$lte" -> toSequenceNr),
       )
-      val queryBuilder = collection
-        .find(query)
-        .hint(collection.hint(BSONDocument(
-          Fields.persistenceId -> 1,
-          Fields.to_sn -> 1,
-          Fields.from_sn -> 1,
-        )))
-      rxDriver.explain(collection)(QueryType.Recovery, queryBuilder)
+      val index = BSONDocument(
+        Fields.persistenceId -> 1,
+        Fields.to_sn -> 1,
+        Fields.from_sn -> 1,
+      )
+      collection.find(query).hint(collection.hint(index))
+    }
 
-      queryBuilder
-        .cursor[BSONDocument]()
-        .collect[List](if (max >= Int.MaxValue) Int.MaxValue else max.intValue())
-        .flatMap { docs =>
-          val eventualsRep = docs
-            .flatMap(_.getAsOpt[Seq[BSONDocument]](Fields.events).get)
-            .map { doc =>
-              val manifest = doc.getAsOpt[String](Fields.manifest).get
-              val rawPayload = doc.getAsOpt[BSONDocument](Fields.payload).get
-              val sequenceNr = doc.getAsOpt[Long](Fields.sequence).get
-              serializer.deserialize(manifest, rawPayload, persistenceId, sequenceNr.toString).map(payload =>
-                PersistentRepr(
-                  payload,
-                  doc.getAsOpt[Long](Fields.sequence).get,
-                  doc.getAsOpt[String](Fields.persistenceId).get,
-                  manifest
-                )
-              ) andThen {
-                case Success(_) => logger.debug(s"[[Roro]] Deserialization completed for event with persistenceId:$persistenceId and sequenceNr:$sequenceNr")
-                case Failure(ex) => logger.error(s"[[Roro]] Deserialization failed for event with persistenceId:$persistenceId and sequenceNr:$sequenceNr", ex)
-              }
-            }
+    def getEvent(document: BSONDocument): Seq[BSONDocument] = document.getAsOpt[Seq[BSONDocument]](Fields.events).get
 
-          Future
-            .sequence(eventualsRep)
-            .map(_.foreach(recoveryCallback))
-        }
+    def deserializeEvents(bsonEvents: Seq[BSONDocument]): Seq[Future[PersistentRepr]] = {
+      bsonEvents.map { bsonEvent =>
+        val manifest = bsonEvent.getAsOpt[String](Fields.manifest).get
+        val rawPayload = bsonEvent.getAsOpt[BSONDocument](Fields.payload).get
+        val sequenceNr = bsonEvent.getAsOpt[Long](Fields.sequence).get
+        serializer.deserialize(manifest, rawPayload, persistenceId, sequenceNr.toString) andThen {
+          case Success(_) => logger.debug(s"[[Roro]] Deserialization completed for event with persistenceId:$persistenceId and sequenceNr:$sequenceNr")
+          case Failure(ex) => logger.error(s"[[Roro]] Deserialization failed for event with persistenceId:$persistenceId and sequenceNr:$sequenceNr", ex)
+        } map (payload => PersistentRepr(
+          payload,
+          bsonEvent.getAsOpt[Long](Fields.sequence).get,
+          bsonEvent.getAsOpt[String](Fields.persistenceId).get,
+          manifest
+        ))
+      }
+    }
 
-    }.map { _ => }
+    for {
+      collection <- rxDriver.journalCollection(persistenceId)
+      query = buildQuery(collection)
+      _ = rxDriver.explain(collection)(QueryType.Recovery, query)
+      documents <- query.cursor[BSONDocument]().collect[List](max.toInt)
+      _ = logger.debug(s"Recovered ${documents.size} for persistenceId:$persistenceId from mongo")
+      bsonEvents = documents.flatMap(getEvent)
+      events <- Future.sequence(deserializeEvents(bsonEvents))
+    } yield events.foreach(recoveryCallback)
+  } andThen {
+    case Success(_) => logger.debug(s"[[Roro]] All events recovered for persistenceId:$persistenceId")
+    case Failure(ex) => logger.error(s"[[Roro]] Failed recovering events for persistenceId:$persistenceId", ex)
   }
-
 
 }
